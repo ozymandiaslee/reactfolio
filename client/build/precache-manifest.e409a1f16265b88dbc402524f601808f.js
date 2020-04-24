61c620a353fb669e37bd6a39b15432fe75c44ff2
Ozymandias <raymondlee@kaffeinatenc.com>
2020-04-23 22:40:21 -0400

    Heroku deploy

---
+++ client/build/precache-manifest.e409a1f16265b88dbc402524f601808f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "712b52c4469f141f7192",
+    "url": "/static/css/main.f61876d7.chunk.css"
+  },
+  {
+    "revision": "712b52c4469f141f7192",
+    "url": "/static/js/main.d59bab5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1868546b76755d13629b",
+    "url": "/static/js/2.cdefec29.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f0f1f47b6e7da92dc5a4bf1bae5e5dbd",
+    "url": "/index.html"
+  }
+];
