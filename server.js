17a97e703d0d0d6db69a979f65dc0b69eb07f6c5
Ozymandias <raymondlee@kaffeinatenc.com>
2020-04-24 00:04:52 -0400

    Heroku

--- server.js
+++ server.js
@@ -17,12 +17,6 @@ if (process.env.NODE_ENV === "production") {
   res.sendFile(path.join(__dirname, "./client/build/"));
 };
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/"));
-});
-
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
